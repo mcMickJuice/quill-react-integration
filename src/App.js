c17506384e34f17eee6ac6e0ea8b7006bbff1487
Mike Joyce <mikejoyce19@gmail.com>
2021-03-18 08:20:56 -0500

    rename App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import React from 'react'
 import QuillWrapper from './QuillWrapper'
 
 export default function App() {
-  const [contents, setContents] = React.useState<any>([])
+  const [contents, setContents] = React.useState([])
 
   return (
     <>
       <div className="App">
         This is a react app that uses quill
-        <QuillWrapper onChange={(c: any) => setContents(c)} />
+        <QuillWrapper onChange={(c) => setContents(c)} />
       </div>
       <pre>{JSON.stringify(contents, null, 2)}</pre>
     </>
